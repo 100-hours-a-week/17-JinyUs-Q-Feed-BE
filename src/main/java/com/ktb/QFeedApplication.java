be9f327b72ff7f22142738bf279b22d6268e6660
mdgome <mdgome3@gmail.com>
2026-01-19 16:23:46 +0900

    build: initial project setup

---
+++ src/main/java/com/ktb/QFeedApplication.java
@@ -0,0 +1,12 @@
+package com.ktb;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QFeedApplication {
+    static void main() {
+        SpringApplication.run(QFeedApplication.class);
+    }
+}
---
+++ src/main/java/com/ktb/auth/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.ktb.auth.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/actuator/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+}
