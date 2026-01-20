b6a86500b7a0c832fedb08fd3ec333622e72889c
mdgome <74102653+mdgome@users.noreply.github.com>
2026-01-20 17:45:26 +0900

    Revert "feat: 파일 도메인 모델 및 예외 계층 추가"
    This reverts commit e01df53154dc793ad38e91dabc880db234518cbe.

--- src/main/java/com/ktb/file/exception/FileAlreadyDeletedException.java
+++
@@ -1,7 +0,0 @@
-package com.ktb.file.exception;
-
-public class FileAlreadyDeletedException extends RuntimeException {
-    public FileAlreadyDeletedException(String message) {
-        super(message);
-    }
-}
