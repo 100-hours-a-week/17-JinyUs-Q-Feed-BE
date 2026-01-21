d7886f78dbf556b56f5e32d54af3d629578c01c8
mdgome <mdgome3@gmail.com>
2026-01-21 19:28:24 +0900

    style: 불필요한 주석 및 파일 제거

--- src/main/java/com/ktb/common/domain/BaseUsableEntity.java
+++ src/main/java/com/ktb/common/domain/BaseUsableEntity.java
@@ -9,14 +9,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-/**
- * 사용 여부(use_yn)를 관리하는 베이스 엔티티
- * - created_at (생성 일시)
- * - updated_at (수정 일시)
- * - use_yn (사용 여부, Soft Disable)
- *
- * 적용 대상: HASHTAG, METRIC
- */
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
