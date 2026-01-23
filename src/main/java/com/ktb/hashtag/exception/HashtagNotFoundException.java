d796bf2db7cabf72ae8c0d36a829584bfda7e932
mdgome <mdgome3@gmail.com>
2026-01-23 14:59:53 +0900

    feat:[#57] 답변 별 키워드 포함 여부 도메인 수정
      - AnswerHashtag.create(): included 파라미터 지원
      - AnswerHashtagRepository, HashtagRepository 추가
      - QuestionHashtagRepository: INNER JOIN FETCH로 N+1 방지
      - ErrorCode: HASHTAG_NOT_FOUND 등 Hashtag 관련 코드 추가
      - HashtagNotFoundException 추가

---
+++ src/main/java/com/ktb/hashtag/exception/HashtagNotFoundException.java
@@ -0,0 +1,12 @@
+package com.ktb.hashtag.exception;
+
+import com.ktb.common.domain.ErrorCode;
+import com.ktb.common.exception.BusinessException;
+
+public class HashtagNotFoundException extends BusinessException {
+
+    public HashtagNotFoundException(Long hashtagId) {
+        super(ErrorCode.HASHTAG_NOT_FOUND,
+              "해시태그를 찾을 수 없습니다: " + hashtagId);
+    }
+}
